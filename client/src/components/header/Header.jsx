8b467975617bc6da15e14bea21fde1e5eb98dda0
MBashov <galacticos.mb@gmail.com>
2025-03-20 20:37:28 +0100

    Show dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,21 +1,31 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import { userContext } from "../../contexts/userContext";
 
 export default function Header() {
+
+    const { email } = useContext(userContext);
+
     return (
         <header>
             <h1><a className="home" href="/">GamesPlay</a></h1>
             <nav>
                 <Link to="/games">All games</Link>
-                {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                {/* <!-- Guest users --> */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {email
+                    ? (
+                        <div id="user">
+                            <Link to="/games/create">Create Game</Link>
+                            <Link to="/logout">Logout</Link>
+                            {email}
+                        </div>
+                    )
+                    : (
+                        <div id="guest">
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </div>
+                    )
+                }
             </nav>
         </header>
     );
